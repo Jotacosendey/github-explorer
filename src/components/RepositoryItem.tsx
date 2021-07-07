81d9b7fc0c125d1b86e60c170c5a07b68cde9c15
JotaCosendey <joaovictor_barros@hotmail.com>
2021-07-07 10:49:01 -0300

    primeiro commit

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,21 @@
+// const repositoryName = 'unform2';
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+
+      <a href={props.repository.html_url}>
+        Acessando repositórios
+    </a>
+    </li>
+  );
+}
